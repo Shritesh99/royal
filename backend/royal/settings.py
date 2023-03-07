5bd86a93e4c03512ca96c40471edecf84c19adf9
Shritesh Jamulkar <shritesh.sj@gmail.com>
2023-03-07 05:04:41 +0000

    Changed Allowed hosts

--- backend/royal/settings.py
+++ backend/royal/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == "True"
 
-ALLOWED_HOSTS = ['*', '0.0.0.0']
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS").split(",")
 
 # Application definition
 
