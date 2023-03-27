a3a1b4f9c28a1a2210bab1a8795328603e889263
Shritesh <shritesh.sj@gmail.com>
2023-03-27 15:14:30 +0100

    Added bithday and gneder support

--- frontend/src/pages/auth.js
+++ frontend/src/pages/auth.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { GoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin, hasGrantedAllScopesGoogle } from "@react-oauth/google";
 import Image from "next/image";
 import { useRecoilState } from "recoil";
 import { useRouter } from "next/router";
 import { ErrorAtom, LoadingAtom } from "../atoms";
 import { Constants } from "../utils";
+import img from "../../public/images/19197921.jpg";
 
 export default function Auth() {
 	const [loading, setLoading] = useRecoilState(LoadingAtom);
@@ -16,36 +17,42 @@ export default function Auth() {
 		}
 		setLoading(false);
 	}, []);
+	const googleLogin = useGoogleLogin({
+		scope: "https://www.googleapis.com/auth/user.birthday.read https://www.googleapis.com/auth/user.gender.read",
+		onSuccess: (response) => {
+			console.log(response);
+			const hasAccess = hasGrantedAllScopesGoogle(
+				response,
+				"https://www.googleapis.com/auth/user.birthday.read",
+				"https://www.googleapis.com/auth/user.gender.read"
+			);
+			if (!hasAccess) {
+				setErr("Not Given Proper Access");
+				return;
+			}
+			localStorage.setItem(
+				Constants.GOOGLE_CLIENT_TOKEN,
+				credentialResponse.credential
+			);
+			router.push("/");
+		},
+	});
+
 	const router = useRouter();
 	return (
 		<div className="container">
 			<div className="columns is-align-items-center">
 				<div className="column is-one-third ">
 					<div className="title is-1">Let's get Started</div>
-					<GoogleLogin
-						onSuccess={(credentialResponse) => {
-							localStorage.setItem(
-								Constants.GOOGLE_CLIENT_TOKEN,
-								credentialResponse.credential
-							);
-							router.push("/");
-						}}
-						onError={() => {
-							setErr("Login Failed");
-						}}
-						useOneTap
-						auto_select
-					/>
+					<button class="button" onClick={() => googleLogin()}>
+						<span class="icon">
+							<i class="fab fa-google"></i>
+						</span>
+						<span>Sign In With Google</span>
+					</button>
 				</div>
 				<div className="column is-two-thirds">
-					<Image
-						src="/images/19197921.jpg"
-						alt="img"
-						width="0"
-						height="0"
-						sizes="100vw"
-						style={{ width: "100%", height: "auto" }}
-					/>
+					<Image src={img} alt="img" />
 				</div>
 			</div>
 		</div>
