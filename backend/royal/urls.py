a2ad4a259b727f131104a3fe00e848f7f6f3f999
Shritesh Jamulkar <shritesh.sj@gmail.com>
2023-03-28 04:07:05 +0100

    Added csrf exempt

--- backend/royal/urls.py
+++ backend/royal/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", GraphQLView.as_view(graphiql=settings.DEBUG)),
+    path("", csrf_exempt(GraphQLView.as_view(graphiql=settings.DEBUG))),
 ]
