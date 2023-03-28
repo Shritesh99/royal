f409f8a53c736871644dd9da051f198855efe36e
Shritesh Jamulkar <shritesh.sj@gmail.com>
2023-03-28 04:09:16 +0100

    fixed questions not visible on admin site

--- backend/questions/admin.py
+++ backend/questions/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from questions.models import *
 
 # Register your models here.
-admin.register(Choice)
-admin.register(GREQuestion)
-admin.register(FSLSMQuestion)
+admin.site.register(Choice)
+admin.site.register(GREQuestion)
+admin.site.register(FSLSMQuestion)
