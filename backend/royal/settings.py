c3893826eb2050135cd41c0bc348cd901f8189c0
Shritesh Jamulkar <shritesh.sj@gmail.com>
2023-03-29 00:28:36 +0100

    changed csrf

--- backend/royal/settings.py
+++ backend/royal/settings.py
@@ -32,6 +32,7 @@ DEBUG = env('DEBUG') == "True"
 ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS").split(",")
 
 CSRF_TRUSTED_ORIGINS = env("DJANGO_ALLOWED_CLIENTS").split(",")
+CSRF_USE_SESSIONS = False
 
 CORS_ALLOWED_ORIGINS = env("DJANGO_ALLOWED_CLIENTS").split(",")
 CORS_ALLOW_ALL_ORIGINS = True
@@ -71,7 +72,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
