141a0777c07f96c9d5577e904922106a8ba5937b
Shritesh <shritesh.sj@gmail.com>
2023-03-29 16:54:39 +0100

    Added 404 page

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import img from "../../public/images/404.jpg";
+
 export default function Custom404() {
-	return <h1>404 - Page Not Found</h1>;
+	return <Image src={img} alt="img" />;
 }
